0954d6fa02a708bbbc95af43dd52fdf67c505f06
Devin P <devinfitzsimons@yahoo.com>
2015-02-22 17:00:24 -0500

    P1 for Udacity Full Stack Nanodegree

---
+++ entertainment_center.py
@@ -0,0 +1,63 @@
+
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie(120,
+	"Toy Story",
+	"A story about a boy and his toys",
+	"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie(128,
+	"Avatar",
+	"A marine on an alien planet.",
+	"http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+big_lebowski = media.Movie(420,
+	"The Big Lebowski",
+	"A dude abides",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/3/35/Biglebowskiposter.jpg/220px-Biglebowskiposter.jpg",
+	"https://www.youtube.com/watch?v=ngV0RBhGZmE")
+
+armageddon = media.Movie(120,
+	"Armageddon",
+	"Oil rig drillers attempt to save the world.",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Armageddon-poster06.jpg/220px-Armageddon-poster06.jpg",
+	"https://www.youtube.com/watch?v=kg_jH47u480")
+
+independence_day = media.Movie(120,
+	"Independence Day",
+	"Aliens invade and humanity fights back",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Independence_day_movieposter.jpg/220px-Independence_day_movieposter.jpg",
+	"https://www.youtube.com/watch?v=kA2WzBi2grE")
+
+amelie = media.Movie(120,
+	"Amelie",
+	"An eccentric French woman finds love",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/5/53/Amelie_poster.jpg/220px-Amelie_poster.jpg",
+	"https://www.youtube.com/watch?v=HEFrLnS5sQY")
+
+rounders = media.Movie(120,
+	"Rounders",
+	"A law student takes on the poker world",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/6/67/RoundersPoster.jpg/220px-RoundersPoster.jpg",
+	"https://www.youtube.com/watch?v=dtJvyTKe1iE")
+
+friends = media.TVShow(30,
+	"Friends",
+	"Six friends live in New York",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/8/86/Friends_titles.jpg/260px-Friends_titles.jpg",
+	"https://www.youtube.com/watch?v=ko-pFBY5kG8",
+	10)
+
+the_wire = media.TVShow(60,
+	"The Wire",
+	"Life and Crime in Baltimore",
+	"http://upload.wikimedia.org/wikipedia/en/thumb/6/62/The_Wire.jpg/250px-The_Wire.jpg",
+	"https://www.youtube.com/watch?v=K3rZgs3dudI",
+	5)
+
+movies = [toy_story, avatar, big_lebowski, armageddon, independence_day, amelie, rounders, friends, the_wire]
+
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,51 @@
+import webbrowser
+
+class Video():
+	"""Inheritable class for video objects"""
+	def __init__(self, duration, title):
+		self.duration = duration
+		self.title = title
+
+	def get_info():
+		print("Title: " + self.title)
+		print("Duration: " + str(self.duration))
+		
+class Movie(Video):
+	""" This class stores movies and related information. Inherits from Video"""
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+	def __init__(self, duration, title, movie_storyline, poster_image, trailer_youtube):
+		Video.__init__(self, duration, title)
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
+
+	def get_info(self):
+		print("Title: " + self.title)
+		print("Duration: " + str(self.duration))
+		print("Storyline: " + self.storyline)
+		print("Poster URL: " + self.poster_image_url)
+		print("Trailer URL" + self.trailer_youtube_url)
+
+class TVShow(Video):
+	"""docstring for TVShow which inherits from Video"""
+	def __init__(self, duration, title, show_synopsis, show_image, theme_song, number_of_seasons):
+		Video.__init__(self, duration, title)
+		self.synopsis = show_synopsis		
+		self.show_image_url = show_image
+		self.theme_song_url = theme_song
+		self.num_seasons = number_of_seasons
+
+	def play_themesong(self):
+		webbrowser.open(self.theme_song_url)
+
+	def get_info(self):
+		print("Title: " + self.title)
+		print("Duration: " + str(self.duration))
+		print("Synopsis: " + self.synopsis)
+		print("Image URL: " + self.show_image_url)
+		print("Theme Song URL: " + self.theme_song_url)
+		print("Number of season: " + str(self.num_seasons))
